4f352d2144f376ac82c7242e98b79fc107bec09c
GrantMelvin <Grantmelvin4@gmail.com>
2024-04-11 14:49:59 -0400

    login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,10 +2,15 @@ import React from "react";
 import { AccountContext } from "../AccountContext";
 import { useContext } from "react" ;
 import { useNavigate } from "react-router";
+import {Input, Box, VStack, InputRightElement, InputGroup, Button, HStack, ButtonGroup} from '@chakra-ui/react'
   
 const Login = () => {
 
     const {user, setUser} = useContext(AccountContext) ;
+
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+    
     const navigate = useNavigate();
 
     const submit = async () => {
@@ -19,27 +24,54 @@ const Login = () => {
         })
         .then(response => response.json())
         .then(data => {
-            localStorage.setItem('token', data.token);
-            setUser({...data})
-        })
-        .then(() => {
-            navigate('/Home')
+            if(data.token){
+                localStorage.setItem('token', data.token);
+                setUser({...data})
+                navigate('/Home')
+            }else{
+                console.log('login failed')
+            }
         })
         .catch(error => console.error('Error:', error));
     }
 
     return (
-        <div>
-        <button
-        onClick={() => {
-            submit()
-        }}>Login</button>
-        {'          '}
-        <button
-        onClick={() => {
-            navigate('/Register')
-        }}>Register</button>
-        </div>
+        <VStack w='100%' h='100%'>
+            
+            <VStack w='20%' h='100%' mt='20%'>
+
+                <Input
+                    pr='4.5rem'
+                    placeholder='Enter Username'
+                />
+
+                <InputGroup size='md'>
+                    <Input
+                        pr='4.5rem'
+                        type={show ? 'text' : 'password'}
+                        placeholder='Enter password'
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+
+            </VStack>
+        
+        <ButtonGroup>
+            <Button
+            onClick={() => {
+                submit()
+            }}>Login</Button>
+            <Button
+            onClick={() => {
+                navigate('/Register')
+            }}>Register</Button>
+        </ButtonGroup>
+        
+        </VStack>
     );
 };
   
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -2,10 +2,15 @@ import React from "react";
 import { AccountContext } from "../AccountContext";
 import { useContext } from "react" ;
 import { useNavigate } from "react-router";
+import {Input, Box, VStack, InputRightElement, InputGroup, Button, HStack, ButtonGroup} from '@chakra-ui/react'
+  
+const Login = () => {
 
-const Register = () => {
+    const {user, setUser} = useContext(AccountContext) ;
 
-    const {setUser} = useContext(AccountContext) ;
+    const [show, setShow] = React.useState(false)
+    const handleClick = () => setShow(!show)
+    
     const navigate = useNavigate();
 
     const submit = async () => {
@@ -21,24 +26,59 @@ const Register = () => {
         .then(data => {
             if(data.success === true){
                 navigate('/Login')
+            }else{
+                // some sort of flag
+                console.log('Register failed')
             }
         })
         .catch(error => console.error('Error:', error));
     }
 
     return (
-        <div>
-        <button
-        onClick={() => {
-            submit()
-        }}>Register</button>
-        {'     '}
-        <button
-        onClick={() => {
-            navigate('Login')
-        }}>Login</button>
-        </div>
+        <VStack w='100%' h='100%'>
+            
+            <VStack w='20%' h='100%' mt='20%'>
+
+                <Input
+                    pr='4.5rem'
+                    placeholder='Enter Username'
+                />
+
+                <InputGroup size='md'>
+                    <Input
+                        pr='4.5rem'
+                        type={show ? 'text' : 'password'}
+                        placeholder='Enter password'
+                    />
+                    <InputRightElement width='4.5rem'>
+                        <Button h='1.75rem' size='sm' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                        </Button>
+                    </InputRightElement>
+                </InputGroup>
+                <InputGroup size='md'>
+                    <Input
+                        pr='4.5rem'
+                        type={show ? 'text' : 'password'}
+                        placeholder='Confirm password'
+                    />
+                </InputGroup>
+
+            </VStack>
+        
+        <ButtonGroup>
+            <Button
+            onClick={() => {
+                submit()
+            }}>Register</Button>
+            <Button
+            onClick={() => {
+                navigate('/Login')
+            }}>Login</Button>
+        </ButtonGroup>
+        
+        </VStack>
     );
 };
   
-export default Register;
+export default Login;
